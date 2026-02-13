fba1087e61a083a165f7b0b1fa2a472035a98705
Nathan Slone <n4llker3nel@protonmail.com>
2026-02-13 03:55:17 +0000

    Update PublicLoS.py

--- Core/PublicLoS.py
+++ Core/PublicLoS.py
@@ -14,7 +14,7 @@ time.sleep(0.5); print("Predecessor: Nokoribi")
 time.sleep(1); print("\nThe first game of Nathan T.S [Tiktok: void_developers]")
 time.sleep(1.5); print("Date of Production: February 12th, 2025 [3:53PM]")
 time.sleep(1.5); print(" GitHub Account: NathanSlone2010")
-time.sleep(1.5); print("  VERS. 3.06.02")
+time.sleep(1.5); print("  VERS. 3.07.02")
 time.sleep(1.5); print("   RATING: 14+")
 time.sleep(1.5); print("    This game is the first ever game of production... Enjoy \n")
 print("\nThe REVOLVERS lay in front of you...")
@@ -25,6 +25,7 @@ while True:
     print("The CYLINDER SPINS...\n")
     time.sleep(1); print("1. Shoot yourself...")
     time.sleep(1); print("2. Shoot the woman....")
+    time.sleep(3); print("3. REVENGE.....")
     choice = input(">>> ")
 
     # Randomize the bullet each turn
@@ -51,3 +52,14 @@ while True:
            time.sleep(1.2); print("You survived, but at what costs...? HOW WILL YOU LIVE WITH YOURSLEF???")
            time.sleep(2.4); print("Come and collect your prize...")
            time.sleep(3); break
+
+    if choice == "3":
+        time.sleep(1); print("You face the guard, looking at him with hatred...\n")
+        if random.random() < odds:
+            print("The gun clicks, clicks, clicks, but doesn't go off... The guard gets angry.....")
+            time.sleep(2); print("...They will be missed...")
+            time.sleep(1.8); break
+        else:
+            print("You point the gub at the guard... Click, the gun fires, the guard's body slumps to the ground...")
+            time.sleep(1.5); print("YOu saved yourself and the woman... But for how long..?")
+            time.sleep(1); break
