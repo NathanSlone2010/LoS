a99b0a4bc30663e927171e1013ddfca5a5ed6515
Nathan Slone <n4llker3nel@protonmail.com>
2026-03-12 23:29:39 -0400

    DELETE THIS FILE ONCE YOUR NEW OS IS SET UP
    FOR OUTSIDERS SEEING THIS. IT'LL REPLACE PublicLoS.py

---
+++ Core/TEMP-REPO-FILE.py
@@ -0,0 +1,46 @@
+import time
+import random
+import sys
+import os
+import platform
+#These are the basic required modules.
+
+
+print("\033[91mNO ERRORS\033[0m")
+#If users see this, program ran correctly
+
+
+os_type = platform.system()
+if os_type == "Windows":
+	print("WINDOWS OPERATING SYSTEM")
+elif os_type == "Darwin":
+	print("APPLE COMPUTER OPERATING SYSTEM")
+elif os_type == "Linux":
+	print("LINUX KERNEL")
+elif os_type in ["FreeBSD", "OpenBSD", "NetBSD"]:
+	print("BSD OPERATING SYSTEM")
+if os_type not in ["Windows", "Darwin", "Linux", "FreeBSD", "OpenBSD", "NetBSD"]:
+	print("REPORT OS TO DEVELOPER FOR PROPER INTEGRATIONS.")
+#This gives users OS recognition and a way to know the code is meant/able to run.
+
+
+time.sleep(1); print("FOR MACIMUM PLAYABILITY, PLAY YOUR OWN MUSIC CHOICES!")
+
+
+cylinder_size = 6
+oods = 4 / cylinder_size #Change this when working on the game.
+days = 0
+count = 0
+#These all make the game functional. Too many to go in-depth with.
+
+
+time.sleep(0.5); print("\nROUNDS ROULETTE")
+time.sleep(0.5); print("Predecessor: Nokoribi")
+time.sleep(0.5); print("\nSecond game of Nathan T.S [Tiktok: void_developer")
+time.sleep(1.5); print("Date of Production: February 12th, 2026 [3:53PM]")
+time.sleep(1.5); print(" Github Account: NathanSlone2010")
+time.sleep(1.5); print("  VERS. 6.10.4")
+time.sleep(1.5); print("   RATING: 14+")
+time.sleep(1.5); print("    TO ACCESS HISTORY; LOG")
+print("The REVOLVER lay in front of you")
+#Standard introduction
